628549747657b6d1b0175cb047af8042eeda7152
sheldon.chang <sheldon.hw.chang@delta.com.tw>
2014-03-13 13:42:10 +0800

    check dout automatically

--- filter/filter.srcs/sim_1/new/filter_tb.v
+++ filter/filter.srcs/sim_1/new/filter_tb.v
@@ -24,15 +24,15 @@ module filter_tb;
 
     reg	clk;
     reg	resetn;
-    reg	[7:0]	din00;
-    reg	[7:0]	din01;
-    reg	[7:0]	din02;
-    reg	[7:0]	din10;
-    reg	[7:0]	din11;
-    reg	[7:0]	din12;
-    reg	[7:0]	din20;
-    reg	[7:0]	din21;
-    reg	[7:0]	din22;
+    wire	[7:0]	din00;
+    wire	[7:0]	din01;
+    wire	[7:0]	din02;
+    wire	[7:0]	din10;
+    wire	[7:0]	din11;
+    wire	[7:0]	din12;
+    wire	[7:0]	din20;
+    wire	[7:0]	din21;
+    wire	[7:0]	din22;
     reg	din_valid;
     wire	[7:0]	dout;				
     wire	dout_valid;
@@ -58,8 +58,17 @@ module filter_tb;
 
     reg [7:0] counter1;
     reg [7:0] counter2;
-        
-    parameter clk_period = 10;
+    
+    reg [7:0] fifo[0:1023];
+    reg [9:0]  rd_ptr;
+    reg [9:0]  wr_ptr;
+    
+    wire [7:0] ans;
+    reg [15:0] temp;
+    
+    reg [8*9-1 : 0] long_adder;
+    
+    parameter clk_period = 2;
     initial clk = 1'b0;
     always #(clk_period/2) clk = ~clk;
     
@@ -71,7 +80,77 @@ module filter_tb;
         
         resetn = 1'b1;
     end
+    
+    integer i;
+    
+    assign ans = fifo[rd_ptr];
+    
+    assign din00 = long_adder[0 +: 8];
+    assign din01 = long_adder[8 +: 8];
+    assign din02 = long_adder[16 +: 8];
+    
+    assign din10 = long_adder[24 +: 8];
+    assign din11 = long_adder[32 +: 8];
+    assign din12 = long_adder[40 +: 8];
+
+    assign din20 = long_adder[48 +: 8];
+    assign din21 = long_adder[56 +: 8];
+    assign din22 = long_adder[64 +: 8];
+    
+    
+    always@(*) begin
+    
+    	// TODO: improve this poor timing design
+    	
+    	/*
+    	 * This is a Gaussian filter
+    	 *
+    	 *	1  2  1
+    	 *  2  4  2
+    	 *  1  2  1
+    	 *
+    	*/ 
+	    temp <= {8'b0, din00} 			+ {7'b0, din01, 1'b0} + {8'b0, din02} 		+  			 
+    			{7'b0, din10, 1'b0} 	+ {6'b0, din11, 2'b0} + {7'b0, din12, 1'b0} +
+    			{8'b0, din20} 			+ {7'b0, din21, 1'b0} + {8'b0, din22} 		;
+    end
+    
+    always@(posedge clk) begin
+    
+        if(resetn == 1'b0) begin
         
+            wr_ptr <= 0;
+            rd_ptr <= 0;
+            
+            for(i = 0; i < 1024; i = i + 1) begin
+                
+                fifo[i] <= 0;                
+            end
+        end
+        else begin
+        
+            if( din_valid == 1'b1 ) begin
+            
+                fifo[wr_ptr] <= temp[4 +: 8];
+                wr_ptr       <= wr_ptr + 1;
+            end
+            
+            if( dout_valid == 1'b1 ) begin
+            
+                rd_ptr <= rd_ptr + 1;
+            end
+        end
+    end
+    
+    always@(posedge clk) begin
+    
+        if( dout_valid == 1'b1 &&
+            dout       != ans     ) begin
+        
+            $display("fail @ %t", $time);
+        end
+    end
+    
     always@(posedge clk) begin
     
         if(resetn == 1'b0) begin
@@ -92,43 +171,41 @@ module filter_tb;
             end
         end
     end
+    
     always@(posedge clk) begin
     
         if(resetn == 1'b0) begin
         
             din_valid <= 1'b0;
             
-            din00 <= 0;
-            din01 <= 0;
-            din02 <= 0;
-            
-            din10 <= 0;
-            din11 <= 0;
-            din12 <= 0;
-            
-            din20 <= 0;
-            din21 <= 0;
-            din22 <= 0;
+            long_adder <= 0;
         end
         else begin
         
             if(counter1 < counter2) begin
+            
                 din_valid <= 1'b1;
                 
-                din00 <= din00 + 1;
-                din01 <= din01 + 1;
-                din02 <= din02 + 1;
-                din10 <= din10 + 1;
-                din11 <= din11 + 1;
-                din12 <= din12 + 1;
-                din20 <= din20 + 1;
-                din21 <= din21 + 1;
-                din22 <= din22 + 1;
+                if(long_adder < 72'hFF_FF_FF_FF_FF_FF_FF_FF_FF) begin
+                
+                    long_adder <= long_adder + 1;
+                end                
             end
-            else begin
-            
+            else begin                                
+                
                 din_valid <= 1'b0;
             end
         end        
     end
+
+    always@(posedge clk) begin
+    
+        if( din22  == 8'hFF  &&
+            wr_ptr == rd_ptr &&
+            dout_valid == 1'b1  ) begin
+        
+            $stop;
+        end
+    end
+    
 endmodule
