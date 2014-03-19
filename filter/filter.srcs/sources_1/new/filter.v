71e9388b8b60cd06fe844c6347952a04abcb46bc
ables3000 <ables3000@gmail.com>
2014-03-19 15:36:31 +0800

    Update filter.v
    the width and height of kernel matrix are changeable.

--- filter/filter.srcs/sources_1/new/filter.v
+++ filter/filter.srcs/sources_1/new/filter.v
@@ -30,20 +30,21 @@ module filter
 		dout,				
 		dout_valid
     );
-	parameter KERNEL_SIZE = 5;
+	parameter KERNEL_WIDTH = 3;
+	parameter KERNEL_HEIGHT = 3;
 	input	clk;
 	input	resetn;
-	input	[KERNEL_SIZE*KERNEL_SIZE*8-1:0]	din;
-	input 	[KERNEL_SIZE*KERNEL_SIZE*6-1:0] kernel;
+	input	[KERNEL_WIDTH*KERNEL_HEIGHT*8-1:0]	din;
+	input 	[KERNEL_WIDTH*KERNEL_HEIGHT*6-1:0] kernel;
 	input	din_valid;
 	output	[31:0]	dout;
 	output	dout_valid;
 	//--------------------------------------------------------------------------
 	
 	
-	parameter total_size = KERNEL_SIZE * KERNEL_SIZE;
-	parameter layer_1_preadder_size = (KERNEL_SIZE/2)*KERNEL_SIZE;
-	parameter layer_1_mul_size = KERNEL_SIZE;
+	parameter total_size = KERNEL_WIDTH * KERNEL_HEIGHT;
+	parameter layer_1_preadder_size = (KERNEL_WIDTH/2)*KERNEL_HEIGHT;
+	parameter layer_1_mul_size = KERNEL_HEIGHT;
 	parameter layer_1_size = layer_1_preadder_size + layer_1_mul_size;
 	parameter adder_tree_layer = $clog2(layer_1_size)+1;
 	parameter latency = 3+adder_tree_layer+1;// layer 1: 3, layer2~  adder tree, the last 1 for output buf
@@ -64,24 +65,24 @@ module filter
 	wire [14:0] layer_1_2 [layer_1_size-1:0];//(a+b)*c	
   
 	generate 	
-		for(i = 0; i < KERNEL_SIZE; i = i + 1) 
+		for(i = 0; i < KERNEL_HEIGHT; i = i + 1) 
 		begin:layer1 // all multiplications are done at layer 1
-			for(j = 0; j < KERNEL_SIZE/2; j = j + 1)
+			for(j = 0; j < KERNEL_WIDTH/2; j = j + 1)
 			begin: layer1_1 // for symmetric coeff, pre-adder dsp48e1 is used.
 				xbip_dsp48_macro_preadder_l3 pre_adder1 (// (A+D)*B  latency 3
 					.CLK(clk),  // input CLK
-					.SEL(kernel_array[0] != kernel_array[KERNEL_SIZE-1]),  // determine (D+A)*B OR (D-A)*B
-					.D(din_array[i*KERNEL_SIZE+j]),      // input [8 : 0] D
-					.B(kernel_array[i*KERNEL_SIZE+j]),      // input [5 : 0] B					
-					.A(din_array[(i+1)*KERNEL_SIZE-j-1]),      // input [8 : 0] A
-					.P(layer_1_2[i*(KERNEL_SIZE/2)+j])      // output [14 : 0] P
+					.SEL(kernel_array[0] != kernel_array[KERNEL_WIDTH-1]),  // determine (D+A)*B OR (D-A)*B
+					.A(din_array[(i+1)*KERNEL_WIDTH-j-1]),      // input [8 : 0] A
+					.B(kernel_array[i*KERNEL_WIDTH+j]),      // input [5 : 0] B					
+					.D(din_array[i*KERNEL_WIDTH+j]),      // input [8 : 0] D
+					.P(layer_1_2[i*(KERNEL_WIDTH/2)+j])      // output [14 : 0] P
 				);		
 			end
 			// the remains are multiply only
 			xbip_dsp48_macro_mul_l3 mul_ABC (//A*B, latency 3
 				.CLK(clk),  // input CLK
-				.A(din_array[i*KERNEL_SIZE+KERNEL_SIZE/2]),      // input [8 : 0] A
-				.B(kernel_array[i*KERNEL_SIZE+KERNEL_SIZE/2]),      // input [5 : 0] B
+				.A(din_array[i*KERNEL_WIDTH+KERNEL_WIDTH/2]),      // input [8 : 0] A
+				.B(kernel_array[i*KERNEL_WIDTH+KERNEL_WIDTH/2]),      // input [5 : 0] B
 				.P(layer_1_2[layer_1_preadder_size+i])      // output [15 : 0] P
 			);
 		end		
