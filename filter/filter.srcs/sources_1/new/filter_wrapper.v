8ac095bf7a934f7480f3791e5652e874b5c9ccaf
ables3000 <ables3000@gmail.com>
2014-03-19 11:38:28 +0800

    Create filter_wrapper.v
    filter_wrapper is responsible for processing SIPO data.

---
+++ filter/filter.srcs/sources_1/new/filter_wrapper.v
@@ -0,0 +1,81 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2014/03/18 18:08:22
+// Design Name: 
+// Module Name: filter_wrapper
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module filter_wrapper(
+		clk,
+		resetn,
+		din,		
+		kernel,		
+		din_valid,
+		dout,				
+		dout_valid
+    );
+	parameter KERNEL_SIZE = 5;
+	input	clk;
+	input	resetn;
+	input	[7:0]	din;
+	input 	[5:0] kernel;
+	input	din_valid;
+	output	[31:0]	dout;
+	output	dout_valid;	
+	//-----------------------------------------------------------------------------
+	
+	reg [KERNEL_SIZE*KERNEL_SIZE*8-1:0]	din_sipo;
+	reg [KERNEL_SIZE*KERNEL_SIZE*6-1:0]	kernel_sipo;
+	reg [KERNEL_SIZE*KERNEL_SIZE-1:0] sipo_valid;
+	
+	integer i;
+	integer j;
+	
+	// serial in parallel out
+	always@( posedge clk) begin
+		if(resetn == 1'b0) begin			
+			din_sipo <= 0;
+			kernel_sipo <= 0;							
+			sipo_valid <= 0;
+		end
+		else begin
+			din_sipo[0+:8] <= din;
+			kernel_sipo[0+:8] <= kernel;			
+			
+			din_sipo[8+:KERNEL_SIZE*KERNEL_SIZE*8-8] <= din_sipo[0+:KERNEL_SIZE*KERNEL_SIZE*8-8];
+			kernel_sipo[6+:KERNEL_SIZE*KERNEL_SIZE*6-6] <= kernel_sipo[0+:KERNEL_SIZE*KERNEL_SIZE*6-6];				
+			
+			sipo_valid <= {sipo_valid, din_valid};
+		end		
+	end
+	
+	
+	
+	filter #(.KERNEL_SIZE(KERNEL_SIZE))	filter_uut
+	(
+		.clk(clk),
+		.resetn(resetn),
+		.din(din_sipo), //[71:0], 9 * 8, 0~255
+		.kernel(kernel_sipo), //[53:0], 9 * 6, -32~31			
+		.din_valid( &sipo_valid ), // valid when all sipo_valid is 1'b1
+		.dout(dout), //[15:0], signed 16bit
+		.dout_valid(dout_valid)		
+	);
+	
+	
+endmodule
