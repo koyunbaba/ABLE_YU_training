10ff29c1d3fba1488b6a4c6a747c69475e2c0bb5
ables3000 <ables3000@gmail.com>
2014-03-19 11:36:31 +0800

    Update filter.v
    symmetric filter with variable size of kerel.
    latency = 3 + adder_tree_layer + 1

--- filter/filter.srcs/sources_1/new/filter.v
+++ filter/filter.srcs/sources_1/new/filter.v
@@ -22,109 +22,106 @@
 //symmetric filter
 module filter
 	(
-		input	clk,
-		input	resetn,
-		input	[71:0]	din,		
-		input 	[53:0] 	kernel,		
-		input	din_valid,
-		output	[15:0]	dout,				
-		output	dout_valid
+		clk,
+		resetn,
+		din,		
+		kernel,		
+		din_valid,
+		dout,				
+		dout_valid
     );
-	parameter latency = 9;// layer 1: 3, layer 2: 3, layer 3~5: 3, 3+3+3 = 9
-    wire [8:0] din_array [8:0];
-    wire [5:0] kernel_array [8:0];
-	wire plus_or_minus [2:0]; // 0 for plus, 1 for minus.
+	parameter KERNEL_SIZE = 5;
+	input	clk;
+	input	resetn;
+	input	[KERNEL_SIZE*KERNEL_SIZE*8-1:0]	din;
+	input 	[KERNEL_SIZE*KERNEL_SIZE*6-1:0] kernel;
+	input	din_valid;
+	output	[31:0]	dout;
+	output	dout_valid;
+	//--------------------------------------------------------------------------
+	
+	
+	parameter total_size = KERNEL_SIZE * KERNEL_SIZE;
+	parameter layer_1_preadder_size = (KERNEL_SIZE/2)*KERNEL_SIZE;
+	parameter layer_1_mul_size = KERNEL_SIZE;
+	parameter layer_1_size = layer_1_preadder_size + layer_1_mul_size;
+	parameter adder_tree_layer = $clog2(layer_1_size)+1;
+	parameter latency = 3+adder_tree_layer+1;// layer 1: 3, layer2~  adder tree, the last 1 for output buf
+	
+    wire [8:0] din_array [total_size-1:0];
+    wire [5:0] kernel_array [total_size-1:0];	
     genvar i;
+	genvar j;
     generate
-        for(i = 0; i < 9; i = i + 1) begin
+        for(i = 0; i < total_size; i = i + 1) begin
             assign din_array[i] = {1'b0, din[i*8 +: 8]}; // for signed num 0~255
             assign kernel_array[i] = kernel[i*6 +: 6]; // -32~31
         end     
-		for(i = 0; i < 3; i = i + 1) begin
-			assign plus_or_minus[i] = (kernel_array[i*3] == kernel_array[i*3+2]) ? 1'b0 : 1'b1;
-		end
-    endgenerate  
-	
+		
+    endgenerate  	
 	
 	reg  [latency-1:0] layer_valid;		
-	wire [14:0] layer_1_2 [2:0];//(a+b)*c
-	reg  [8:0]  layer_1_2_buf [3*3-1:0];// din_array 1 4 7  buffer (din1 -> 0->1->2) (din3 -> 3->4->5) (din5 -> 6->7->8) latency of layer 1 is 3
-	wire [15:0] layer_2_3 [2:0];// a*b+c result, latency of layer 3 is 3		
-	reg  [15:0] layer_3_4 [1:0];
-	reg  [15:0] layer_4_5;
-	reg  [15:0] layer_5_end;	
-	
-	assign dout = layer_5_end;
-	assign dout_valid = layer_valid[latency-1]; 
+	wire [14:0] layer_1_2 [layer_1_size-1:0];//(a+b)*c	
   
 	generate 	
-		for(i = 0; i < 3; i = i + 1) 
-		begin:layer1 // (0 +/- 2)*kernel   (3 +/- 5)*kernel  (6 +/- 8)*kernel
-		     xbip_dsp48_macro_preadder_l3 pre_adder (// (A+D)*B  latency 3
-                .CLK(clk),  // input CLK
-                .A(din_array[i*3]),      // input [8 : 0] A
-                .B(kernel_array[i*3]),      // input [5 : 0] B
-                .D(plus_or_minus[i] == 1'b0 ? din_array[i*3+2] : -din_array[i*3+2]),      // input [8 : 0] D
-                .P(layer_1_2[i])      // output [14 : 0] P
-            );			
-		end
-
-		for(i = 0; i < 3; i = i + 1) 
-		begin:layer2// (1, 4, 7)*kernel + pre-result
-			xbip_dsp48_macro_macc_l3 macc_ABC (//A*B+C, latency 3
+		for(i = 0; i < KERNEL_SIZE; i = i + 1) 
+		begin:layer1 // all multiplications are done at layer 1
+			for(j = 0; j < KERNEL_SIZE/2; j = j + 1)
+			begin: layer1_1 // for symmetric coeff, pre-adder dsp48e1 is used.
+				xbip_dsp48_macro_preadder_l3 pre_adder1 (// (A+D)*B  latency 3
+					.CLK(clk),  // input CLK
+					.SEL(kernel_array[0] != kernel_array[KERNEL_SIZE-1]),  // determine (D+A)*B OR (D-A)*B
+					.D(din_array[i*KERNEL_SIZE+j]),      // input [8 : 0] D
+					.B(kernel_array[i*KERNEL_SIZE+j]),      // input [5 : 0] B					
+					.A(din_array[(i+1)*KERNEL_SIZE-j-1]),      // input [8 : 0] A
+					.P(layer_1_2[i*(KERNEL_SIZE/2)+j])      // output [14 : 0] P
+				);		
+			end
+			// the remains are multiply only
+			xbip_dsp48_macro_mul_l3 mul_ABC (//A*B, latency 3
 				.CLK(clk),  // input CLK
-				.A(layer_1_2_buf[i*3+2]),      // input [8 : 0] A
-				.B(kernel_array[i*3+1]),      // input [5 : 0] B
-				.C(layer_1_2[i]),      // input [14 : 0] C
-				.P(layer_2_3[i])      // output [15 : 0] P
+				.A(din_array[i*KERNEL_SIZE+KERNEL_SIZE/2]),      // input [8 : 0] A
+				.B(kernel_array[i*KERNEL_SIZE+KERNEL_SIZE/2]),      // input [5 : 0] B
+				.P(layer_1_2[layer_1_preadder_size+i])      // output [15 : 0] P
 			);
 		end		
-		
     endgenerate
     
-    integer j;    
+	// adder tree
+	
+	
+	parameter adder_tree_data_size = (2**adder_tree_layer);	
+	reg [31:0] adder_tree_data [adder_tree_data_size-1:0];	// array tree structure
+	reg  [31:0] layer_end;		
+	assign dout = layer_end;
+	assign dout_valid = layer_valid[latency-1]; 
+    integer k; 
     always@(posedge clk) begin    
         
     	if( resetn == 1'b0 ) begin    
-			layer_valid <= 0;
-    		
-			
-			for(j = 0; j < 9; j = j + 1)begin
-				layer_1_2_buf[j] <= 9'b0;							
+			layer_valid <= 0;    		
+			layer_end <= 0;
+			for(k = 0; k < adder_tree_data_size; k = k + 1) begin
+				adder_tree_data[k] <= 0;
 			end		
-			
-			for(j = 0; j < 2; j = j + 1)begin						
-				layer_3_4[j] <= 16'b0;			
-			end		
-			layer_4_5 <= 16'b0;			
-			layer_5_end <= 16'b0;
 			
     	end
-    	else begin
-			//layer 1 buffer for 1 4 7 
-			for(j = 0; j < 3; j = j + 1) begin
-				layer_1_2_buf[j*3] <= din_array[j*3+1];		
-				layer_1_2_buf[j*3+1] <= layer_1_2_buf[j*3];
-				layer_1_2_buf[j*3+2] <= layer_1_2_buf[j*3+1];
-			end		
+    	else begin			
 		
 			//adder tree
-			//layer 3
-			layer_3_4[0] <= layer_2_3[0] + layer_2_3[1];
-			layer_3_4[1] <= layer_2_3[2];			
+			//layer 2
+			for(k = 0; k < layer_1_size; k = k + 1) begin
+				adder_tree_data[adder_tree_data_size/2+k] <= {{17{layer_1_2[k][14]}}, layer_1_2[k]};
+			end			
 			
-			//layer 4
-			layer_4_5 <= layer_3_4[0] + layer_3_4[1];
 			
-			//layer 5
-			layer_5_end <= layer_4_5;
+			//layer 3 ~ ...
+			for(k = 1; k < adder_tree_data_size/2; k = k + 1) begin
+				adder_tree_data[k] <= adder_tree_data[k*2]+adder_tree_data[k*2+1];
+			end
+	   	    layer_end <= adder_tree_data[1];
 			
-			//layer_valid <= {layer_valid, din_valid};
 			layer_valid <= {layer_valid, din_valid};
-	   	    /*layer_valid[0] <= din_valid;
-		    for(j = 1; j < latency; j = j + 1)begin
-				layer_valid[j] <= layer_valid[j-1];		
-            end*/
         end
     end	
 	
