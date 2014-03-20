7409871f910265e597c61fd691a1672731867f8f
ables3000 <ables3000@gmail.com>
2014-03-20 18:54:44 +0800

    Update filter_tb.v
    test for
        raw data: m * n matrix
        kernel data: n * n matrix
        input data: n * 1 matrix
    first initialize kernel matrix.
    then preparing data input.
    finally enable data_transmit_en

--- filter/filter.srcs/sim_1/new/filter_tb.v
+++ filter/filter.srcs/sim_1/new/filter_tb.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2014/03/19 14:00:00
+// Create Date: 2014/03/19 16:06:09
 // Design Name: 
 // Module Name: filter_tb
 // Project Name: 
@@ -20,58 +20,46 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module filter_tb;
-	parameter KERNEL_WIDTH = 7;
-	parameter KERNEL_HEIGHT = 5;
+	parameter KERNEL_SIZE = 3;	
+	parameter DATA_NUM = 20;
 	//-------------------------------------------------------
-`define PISOs
-
-	parameter DATA_IN_SIZE = KERNEL_WIDTH*KERNEL_HEIGHT*8;
-	parameter KERNEL_DATA_SIZE = KERNEL_WIDTH*KERNEL_HEIGHT*6;
+	parameter DATA_IN_SIZE = KERNEL_SIZE*KERNEL_SIZE*8;
+	parameter KERNEL_DATA_SIZE = KERNEL_SIZE*KERNEL_SIZE*6;
+	parameter DATA_INPUT_SIZE = KERNEL_SIZE*(DATA_NUM+2)*8;
     reg		clk;	
     reg		resetn;
-    wire	[DATA_IN_SIZE-1:0]	din;		
-	reg signed	[KERNEL_DATA_SIZE-1:0] 	kernel;	
-    reg		din_valid;
+	reg		row_neg;
+	reg		col_neg;
+    reg	[DATA_IN_SIZE-1:0]	din;		
+	reg signed	[KERNEL_DATA_SIZE-1:0] 	kernel;	    
     wire signed	[31:0]	dout;				
     wire	dout_valid;
 
-`ifdef PISO	 
-	reg [7:0] din_piso [KERNEL_WIDTH*KERNEL_HEIGHT-1+1:0];
-	reg [6:0] kernel_piso [KERNEL_WIDTH*KERNEL_HEIGHT-1+1:0];
-	reg [KERNEL_WIDTH*KERNEL_HEIGHT-1+1:0] piso_valid;
-	wire [7:0] din_piso_result;
-	wire [7:0] kernel_piso_result;
-	wire piso_valid_result;
-`endif	
+	reg		din_valid;
+	reg [8*KERNEL_SIZE-1:0] din_piso_result;
 	
-	
-`ifdef PISO	
-	filter_wrapper #(.KERNEL_WIDTH(KERNEL_WIDTH), .KERNEL_HEIGHT(KERNEL_HEIGHT))	filter_wrapper_uut
+	reg update_kernel;
+	wire [5:0] kernel_piso_result;		
+	wire kernel_piso_valid_result;
+
+	filter_wrapper #(.KERNEL_SIZE(KERNEL_SIZE))	filter_wrapper_uut
 	(
 		.clk(clk),
 		.resetn(resetn),
+		.col_neg(col_neg),
+		.row_neg(row_neg),
 		.din(din_piso_result),		
-		.kernel(kernel_piso_result),		
-		.din_valid(piso_valid_result),
+		.kernel(kernel_piso_result),	
+		.kernel_valid(kernel_piso_valid_result),	
+		.din_valid(din_valid),
 		.dout(dout),				
 		.dout_valid(dout_valid)
     );
-`else
-	filter #(.KERNEL_WIDTH(KERNEL_WIDTH), .KERNEL_HEIGHT(KERNEL_HEIGHT)) filter_uut 
-	(
-        .clk(clk),
-        .resetn(resetn),
-        .din(din), //[71:0], 9 * 8, 0~255
-        .kernel(kernel), //[53:0], 9 * 6, -32~31			
-        .din_valid(din_valid),
-        .dout(dout), //[15:0], signed 16bit
-        .dout_valid(dout_valid)		
-    );
-`endif	
 	
-	
+	reg data_tranmit_en;
     reg [7:0] counter1;
     reg [7:0] counter2;
+	reg [7:0] counter3;
     
     reg [31:0] fifo[0:1023];
     reg [9:0]  rd_ptr;
@@ -79,7 +67,7 @@ module filter_tb;
     
     wire signed [31:0] ans;
     reg [31:0] temp;    
-    reg [DATA_IN_SIZE-1 : 0] long_adder;
+    reg [DATA_INPUT_SIZE-1 : 0] long_adder;
     
     parameter clk_period = 10;
     initial clk = 1'b0;
@@ -90,21 +78,30 @@ module filter_tb;
     initial begin
         
         resetn = 1'b0;
-`ifdef PISO			
-		piso_valid = 0;
-`endif		
-		kernel = {5{6'd1, -6'd16, 6'd1, 6'd31, -6'd1, 6'd16, -6'd1}};
+		update_kernel = 1'b0;
+		row_neg = 1'b0;
+		col_neg = 1'b0;	
+		din_valid = 1'b0;		
+		data_tranmit_en = 1'b0;
+		kernel = {3{-6'd1, 6'd2, -6'd1}}; 
 				  
-        #100
-        
+        #100        
         resetn = 1'b1;
+		#(clk_period)
+		update_kernel <= 1'b1;
+		#(clk_period)
+		update_kernel <= 1'b0;
+		#(clk_period*KERNEL_SIZE*KERNEL_SIZE)
+		#(clk_period/2)
+		data_tranmit_en <= 1'b1;
     end
     
     integer i;
     integer j;
     assign ans = fifo[rd_ptr];
+	reg ans_valid;
     
-    assign din = long_adder[0 +: DATA_IN_SIZE];
+    //assign din = long_adder[0 +: DATA_IN_SIZE];
 
 	reg [31:0] sum;
 	reg [14:0] sum_temp;
@@ -113,7 +110,7 @@ module filter_tb;
         
         // golden answer preparing
 		sum = 0;		
-		for(i = 0; i < KERNEL_WIDTH*KERNEL_HEIGHT; i = i + 1) begin
+		for(i = 0; i < KERNEL_SIZE*KERNEL_SIZE; i = i + 1) begin
 			sum_temp = { 7'b0, {din[i*8 +: 8]} } * { {9{kernel[i*6+5]}}, kernel[i*6+:6]}; // signed mul  din * kernel
 			sum = sum + {{17{sum_temp[14]}}, sum_temp};			
 		end				
@@ -134,7 +131,7 @@ module filter_tb;
         end
         else begin
                 
-            if( din_valid == 1'b1 ) begin
+            if( ans_valid == 1'b1 ) begin
             
                 // answer push-in
                 fifo[wr_ptr] <= temp;
@@ -159,99 +156,111 @@ module filter_tb;
         end
     end
     
-    // counter for din_valid switching
+    // counter1 for data transmition		
     always@(posedge clk) begin
     
         if(resetn == 1'b0) begin
-        
-            counter1 <= 0;
-            counter2 <= 0;
+			din_valid <= 1'b0;
+			ans_valid <= 1'b0;
+            counter1 <= 0;           
+			din_piso_result <= 0;
+			long_adder <= 0;
+			//  	1	2 	3
+			// [	4	5 	6    
+			//  	7	8 	9
+			//  	10	11 	12
+			
         end
-        else begin
-                                
-            if(counter1 == counter2) begin
-            
-                counter1 <= 0;
-                counter2 <= counter2 + 1;
-            end
-`ifdef PISO
-            else if( |piso_valid == 0) begin            
-`else
-			else begin
-`endif			
-                counter1 <= counter1 + 1;
-            end
-
-        end
-    end
-    
-    // test data generate
-    // 
-    // test all combination of din (100% coverage)
-    //
-    // FIXME: very long simulation time and cause memory fill up... 
-    //
-    // TODO: generate more effective data set
-    //
-    always@(posedge clk) begin
-    
-        if(resetn == 1'b0) begin
-        
-            din_valid <= 1'b0;
-            
-            long_adder <= 0;
-        end
-        else begin
-`ifdef PISO		
-			if(|piso_valid == 1'b0) begin
-`endif			
-				if(counter1 < counter2) begin				
-					din_valid <= 1'b1;					
-					long_adder <= long_adder + {5{56'h01_02_03_04_05_06_07}};					
-				end
-				else begin                                					
+        else begin            
+			if(data_tranmit_en == 1'b1) begin				
+				if(counter1 > DATA_NUM+1) begin
+					data_tranmit_en <= 1'b0;
+					counter1 <= 0;
 					din_valid <= 1'b0;
+					ans_valid <= 1'b0;
 				end
-`ifdef PISO				
-			end
+				else begin
+					din_valid <= 1'b1;
+					counter1 <= counter1 + 1;
+					din_piso_result <= long_adder[counter1*KERNEL_SIZE*8 +: 8*KERNEL_SIZE];	
+					if(counter1 < 2) begin
+						din <= long_adder[0 +: DATA_IN_SIZE];
+						ans_valid <= 1'b0;
+					end
+					else begin
+						din <= long_adder[(counter1-2)*KERNEL_SIZE*8 +: DATA_IN_SIZE];
+						ans_valid <= 1'b1;
+					end					
+				end
+			end       
 			else begin
+				counter1 <= 0;
 				din_valid <= 1'b0;
+				ans_valid <= 1'b0;
+				din_piso_result <= 0;
 			end
-`endif						
-        end        
+        end
     end
 	
-`ifdef PISO	
-	// test data parallel in serial out
-	assign din_piso_result = din_piso[KERNEL_WIDTH*KERNEL_HEIGHT];
-	assign kernel_piso_result = kernel_piso[KERNEL_WIDTH*KERNEL_HEIGHT];
-	assign piso_valid_result = piso_valid[KERNEL_WIDTH*KERNEL_HEIGHT];
+	// counter2,3 for enable data transmition		
+    always@(posedge clk) begin
+	
+		if(resetn == 1'b0) begin
+			counter2 <= 0;
+			counter3 <= 10;
+		end
+		else begin
+			if(data_tranmit_en == 1'b0)	 begin
+				if(counter2 == counter3) begin
+					counter2 <= 0;
+					counter3 <= counter3 + 1;
+					data_tranmit_en = 1'b1;
+					for(i = 0; i < KERNEL_SIZE*(DATA_NUM+2); i = i + 1) begin
+						long_adder[i*8 +: 8] <= $random % 256;
+					end
+				end
+				else begin 
+					counter2 <= counter2 + 1;
+				end
+				
+				if(counter3 > 30) begin
+					counter3 <= 10;
+				end			
+				
+			end
+		end
+			
+	end
+	    
+	
+	// initial kernel matrix	
+	reg [5:0] kernel_piso [KERNEL_SIZE*KERNEL_SIZE-1+1:0];	
+	reg [KERNEL_SIZE*KERNEL_SIZE-1+1:0] kernel_piso_valid;
+	assign kernel_piso_valid_result = kernel_piso_valid[KERNEL_SIZE*KERNEL_SIZE];
+	assign kernel_piso_result = kernel_piso[KERNEL_SIZE*KERNEL_SIZE];
 	always@(posedge clk) begin
     
         if(resetn == 1'b0) begin
-			piso_valid <= 0;
-			for(i = 0; i < KERNEL_WIDTH*KERNEL_HEIGHT+1; i = i + 1) begin
-				din_piso[i] <= 0;
+			update_kernel <= 1'b0;
+			kernel_piso_valid <= 0;
+			for(i = 0; i < KERNEL_SIZE*KERNEL_SIZE+1; i = i + 1) begin				
 				kernel_piso[i] <= 0;
 			end			
         end
-        else begin
-        
-            if(din_valid == 1'b1) begin //initial the parallel in values		
-				for(i = 0; i < KERNEL_HEIGHT; i = i + 1) begin
-					for(j = 0; j < KERNEL_WIDTH; j = j + 1) begin
-						din_piso[i*KERNEL_WIDTH+j] <= din[(i*KERNEL_WIDTH+j)*8 +: 8];
-						kernel_piso[i*KERNEL_WIDTH+j] <= kernel[(i*KERNEL_WIDTH+j)*6 +: 6];
-						piso_valid[i*KERNEL_WIDTH+j] <= 1'b1;
+        else begin        
+            if(update_kernel == 1'b1) begin //initial the parallel in values		
+				for(i = 0; i < KERNEL_SIZE; i = i + 1) begin
+					for(j = 0; j < KERNEL_SIZE; j = j + 1) begin						
+						kernel_piso[i*KERNEL_SIZE+j] <= kernel[(i*KERNEL_SIZE+j)*6 +: 6];
+						kernel_piso_valid[i*KERNEL_SIZE+j] <= 1'b1;
 					end
 				end		
-				din_valid <= 1'b0;
+				update_kernel <= 1'b0;
             end
             else begin
-				if( (|piso_valid) == 1'b1) begin // serial out	until piso_valid is 0				
-					piso_valid <= {piso_valid, 1'b0};
-					for(i = 1; i < KERNEL_WIDTH*KERNEL_HEIGHT+1; i = i + 1) begin
-						din_piso[i] <= din_piso[i-1];
+				if( (|kernel_piso_valid) == 1'b1) begin // serial out	until piso_valid is 0				
+					kernel_piso_valid <= {kernel_piso_valid, 1'b0};
+					for(i = 1; i < KERNEL_SIZE*KERNEL_SIZE+1; i = i + 1) begin						
 						kernel_piso[i] <= kernel_piso[i-1];						
 					end
 				end
@@ -259,6 +268,5 @@ module filter_tb;
             end
         end        
     end
-`endif    
-    
+	
 endmodule
