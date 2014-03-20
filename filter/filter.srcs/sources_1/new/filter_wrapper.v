2e18abbdc6af36358ff86032860067a98256f29a
ables3000 <ables3000@gmail.com>
2014-03-20 18:52:06 +0800

    Update filter_wrapper.v
    kernel matrix must to be initialized before computing.
    raw data: m * n matrix
    kernel data: n * n matrix
    input data: n * 1 matrix
    output data: m-2 byte
    latency is 12 when n is 3

--- filter/filter.srcs/sources_1/new/filter_wrapper.v
+++ filter/filter.srcs/sources_1/new/filter_wrapper.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2014/03/18 18:08:22
+// Create Date: 2014/03/19 16:23:09
 // Design Name: 
 // Module Name: filter_wrapper
 // Project Name: 
@@ -19,30 +19,34 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module filter_wrapper(
 		clk,
 		resetn,
-		din,		
+		col_neg,
+		row_neg,
+		din,	
+		din_valid, 		
 		kernel,		
-		din_valid, 
+		kernel_valid,
 		dout,				
 		dout_valid
     );
-	parameter KERNEL_WIDTH = 7;
-	parameter KERNEL_HEIGHT = 5;
+	parameter KERNEL_SIZE = 3;
 	input	clk;
 	input	resetn;
-	input	[7:0]	din;
+	input	col_neg;
+	input 	row_neg;
+	input	[8*KERNEL_SIZE-1:0]	din;
 	input 	[5:0] kernel; 
 	input	din_valid;
+	input	kernel_valid;
 	output	[31:0]	dout;
 	output	dout_valid;	
 	//-----------------------------------------------------------------------------
 	
-	reg [KERNEL_WIDTH*KERNEL_HEIGHT*8-1:0]	din_sipo;
-	reg [KERNEL_WIDTH*KERNEL_HEIGHT*6-1:0]	kernel_sipo;
-	reg [KERNEL_WIDTH*KERNEL_HEIGHT-1:0] sipo_valid;
+	reg [KERNEL_SIZE*KERNEL_SIZE*8-1:0]	din_sipo;
+	reg [KERNEL_SIZE*KERNEL_SIZE*6-1:0]	kernel_sipo;
+	reg [KERNEL_SIZE-1:0] sipo_valid;
 	
 	integer i;
 	integer j;
@@ -55,22 +59,27 @@ module filter_wrapper(
 			sipo_valid <= 0;
 		end
 		else begin
-			din_sipo[0+:8] <= din;
-			kernel_sipo[0+:8] <= kernel;			
+			din_sipo[(KERNEL_SIZE-1)*KERNEL_SIZE*8 +: 8*KERNEL_SIZE] <= din;								
+			din_sipo[0 +: (KERNEL_SIZE-1)*KERNEL_SIZE*8] <= din_sipo[KERNEL_SIZE*8 +: (KERNEL_SIZE-1)*KERNEL_SIZE*8];
 			
-			din_sipo[8+:KERNEL_WIDTH*KERNEL_HEIGHT*8-8] <= din_sipo[0+:KERNEL_WIDTH*KERNEL_HEIGHT*8-8];
-			kernel_sipo[6+:KERNEL_WIDTH*KERNEL_HEIGHT*6-6] <= kernel_sipo[0+:KERNEL_WIDTH*KERNEL_HEIGHT*6-6];				
+			if(kernel_valid == 1'b1) begin
+				kernel_sipo[0+:8] <= kernel;	
+				kernel_sipo[6+:KERNEL_SIZE*KERNEL_SIZE*6-6] <= kernel_sipo[0+:KERNEL_SIZE*KERNEL_SIZE*6-6];							
+			end
 			
 			sipo_valid <= {sipo_valid, din_valid};
+			
 		end		
 	end
 	
 	
 	
-	filter #(.KERNEL_WIDTH(KERNEL_WIDTH), .KERNEL_HEIGHT(KERNEL_HEIGHT))	filter_uut
+	filter #(.KERNEL_SIZE(KERNEL_SIZE))	filter_uut
 	(
 		.clk(clk),
 		.resetn(resetn),
+		.col_neg(col_neg),
+		.row_neg(row_neg),
 		.din(din_sipo), //[71:0], 9 * 8, 0~255
 		.kernel(kernel_sipo), //[53:0], 9 * 6, -32~31			
 		.din_valid( &sipo_valid ), // valid when all sipo_valid is 1'b1
